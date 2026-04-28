c2a6ca97f1854d02771193000985619dea6d5f3b
GABRIEL MARTINEZ LOUFER <49960445@ort>
2026-04-28 07:51:44 -0300

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP1bis_Martinez_Waserman.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
